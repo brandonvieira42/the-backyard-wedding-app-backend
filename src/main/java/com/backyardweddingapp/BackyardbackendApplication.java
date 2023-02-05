d21d4bd81e8d840369486df6f8e1125c5e7db787
BrandonVieira-C <brandon.c.vieira@gmail.com>
2023-02-04 20:35:11 -0500

    cors update

--- src/main/java/com/backyardweddingapp/BackyardbackendApplication.java
+++ src/main/java/com/backyardweddingapp/BackyardbackendApplication.java
@@ -24,7 +24,7 @@ public class BackyardbackendApplication {
 	 public CorsFilter corsFilter() {
 	 	CorsConfiguration corsConfiguration = new CorsConfiguration();
 	 	corsConfiguration.setAllowCredentials(true);
-	 	corsConfiguration.setAllowedOrigins(Arrays.asList(""));
+	 	corsConfiguration.setAllowedOrigins(Arrays.asList("localhost:4200"));
 	 	corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type", 
 	 			"Accept", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control0Request-Method", "Access-Control-Request-Headers"));
 	 	corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization", "Access-Control-Allow-Origin", 
